845d13ddfff22e759999267a9e1af881e88c6d35
Reiz Janto <betswanky@gmail.com>
2024-05-20 23:31:37 +0100

    Mad tapping getting in shape

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,46 @@
+import { useState } from "react";
 import "./App.css";
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { Counter } from "./components/Counter";
-import { Jetton } from "./components/Jetton";
-import { TransferTon } from "./components/TransferTon";
-import styled from "styled-components";
-import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
-import { useTonConnect } from "./hooks/useTonConnect";
-import { CHAIN } from "@tonconnect/protocol";
-import "@twa-dev/sdk";
+import { helix } from 'ldrs'
 
-const StyledApp = styled.div`
-  background-color: #e8e8e8;
-  color: black;
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #222;
-    color: white;
-  }
-  min-height: 100vh;
-  padding: 20px 20px;
-`;
-
-const AppContainer = styled.div`
-  max-width: 900px;
-  margin: 0 auto;
-`;
+helix.register()
 
 function App() {
-  const { network } = useTonConnect();
+  const [helixSize, setHelixSize] = useState(60);
 
   return (
-    <p>MadTap</p>
+    <>
+      <div className="w-full bg-[#092537] h-[10vh]">
+        <div className="flex items-center justify-between h-full px-4">
+          <h1 className="text-white text-4xl font-bold">MADTAP</h1>
+          
+          <h1 className="text-white text-md font-bold">245678 POINTS</h1>
+        </div>
+      </div>
+      <div className="w-full h-[80vh] bg-cover bg-center" style={{ backgroundImage: `url('/src/assets/madtapisto.png')` }}>
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-white text-4xl font-bold">TAP TO PLAY</h1>
+          <div className="flex items-center justify-center w-auto" onClick={() => {
+            console.log(helixSize)
+            if (helixSize === 100) {
+              setHelixSize(60)
+            } else {
+              setHelixSize(helixSize + 2)
+            }
+          }}>
+            <l-helix
+              size={`${helixSize}`}
+              speed="2.5" 
+              color="white" 
+            ></l-helix>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-[#092537] h-[10vh]">
+        <div className="flex items-center justify-center h-full">
+          <h1 className="text-white text-4xl font-bold">Bare Rotten Init!</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
