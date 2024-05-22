8c127aa44b1d9f6e0bfb4d932230b59a2e3368f8
Reiz Janto <betswanky@gmail.com>
2024-05-23 00:00:11 +0100

    Range value checker fixed

--- src/components/Home/AnimatedBackground.tsx
+++ src/components/Home/AnimatedBackground.tsx
@@ -60,6 +60,7 @@ const AnimatedBackground = () => {
   const [pointsNo, setPointsNo] = useState(1);
   const [rangeValue, setRangeValue] = useState(1);
   const [bgGradient, setBgGradient] = useState('bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500')
+  const [currentView, setCurrentView] = useState('play')
 
   
   const prevRangeValueRef = useRef(rangeValue);
@@ -70,10 +71,11 @@ const AnimatedBackground = () => {
       const hasIncreased = rangeValue > prevRangeValue;
       console.log(hasIncreased ? 'Value has increased' : 'Value has not increased');
       if (!hasIncreased && rangeValue !== 0) {
-        setRangeValue(rangeValue - 1)
-        if (pointsNo !== 1 && rangeValue === 0) {
+        if (pointsNo !== 1 && rangeValue === 1) {
           setPointsNo (pointsNo - 1)
           setRangeValue(100)
+        } else {
+          setRangeValue(rangeValue - 1)
         }
       }
 
@@ -97,40 +99,65 @@ const AnimatedBackground = () => {
 
   return (
     <div className={`w-full h-screen ${bgGradient} animate-moveBackground bg-[length:200%_200%] bg-cover bg-center`}>
-      <div className="flex gap-4 items-center justify-center w-full h-[20vh] px-4 py-6">
-        <GoldCoinIcon />
-        <h1 className="text-white text-2xl font-bold">245678</h1>
-      </div>
-      <div className="flex flex-col items-center justify-center w-full h-[50vh] gap-5">
-        <h1 className="text-white text-xs font-bold p-2 border border-[#FFF] rounded-full">TAP BELOW TO PLAY</h1>
-        <div className="flex items-center justify-center w-auto" onClick={() => {
-          changeColor()
-          changeBgGradient()
-          if (rangeValue === 100) {
-            setRangeValue(0)
-            setPointsNo(pointsNo + 1)
-          } else {
-            setRangeValue(rangeValue + 1)
-          }
-          
-        }}>
-          <FloatingPlusOne helixColor={helixColor} pointsNo={pointsNo} />
-        </div>
-        <div className="flex items-center justify-center w-full">
-          <RangeInput rangeValue={rangeValue} />
-        </div>
-      </div>
+      {
+        currentView === 'play' &&
+        <>
+          <div className="flex gap-4 items-center justify-center w-full h-[20vh] px-4 py-6">
+            <GoldCoinIcon />
+            <h1 className="text-white text-2xl font-bold">245678</h1>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-[50vh] gap-5">
+            <h1 className="text-white text-xs font-bold p-2 border border-[#FFF] rounded-full">TAP BELOW TO PLAY</h1>
+            <div className="flex items-center justify-center w-auto" onClick={() => {
+              changeColor()
+              changeBgGradient()
+              if (rangeValue === 100) {
+                setRangeValue(0)
+                setPointsNo(pointsNo + 1)
+              } else {
+                setRangeValue(rangeValue + 1)
+              }
+              
+            }}>
+              <FloatingPlusOne helixColor={helixColor} pointsNo={pointsNo} />
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <RangeInput rangeValue={rangeValue} />
+            </div>
+          </div>
+        </>
+      }
+
+      {
+        currentView === 'referrals' &&
+        <>
+          <p className="flex items-center justify-center text-white">Referrals</p>
+        </>
+      }
+
+      {
+        currentView === 'leaderboard' &&
+        <>
+          <p className="flex items-center justify-center text-white">Leaderboard</p>
+        </>
+      }
       <div className="w-[90%] mx-auto h-[12vh] border border-[#FFF] rounded-full">
         <div className="flex items-center justify-center h-full">
-          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3 rounded-l-full">
+          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3 rounded-l-full" onClick={() => {
+            setCurrentView('play')
+          }}>
             <GoldPlayIcon />
             <span>PLAY</span>
           </div>
-          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3">
+          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3" onClick={() => {
+            setCurrentView('referrals')
+          }}>
             <GoldBarChartIcon />
             <span>REFERRALS</span>
           </div>
-          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3 rounded-r-full">
+          <div className="flex flex-col gap-1 items-center justify-center cursor-pointer text-xs border border-[#FFF] px-1 h-full text-[#FFF] w-1/3 rounded-r-full" onClick={() => {
+            setCurrentView('leaderboard')
+          }}>
             <GoldTrophyIcon />
             <span>LEADERBOARD</span>
           </div>
