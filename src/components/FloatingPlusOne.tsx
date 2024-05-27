906f5d3b87c0b2c25148220664288101af58fc36
Reiz Janto <betswanky@gmail.com>
2024-05-27 21:09:22 +0100

    Add illustration width

--- src/components/FloatingPlusOne.tsx
+++ src/components/FloatingPlusOne.tsx
@@ -47,7 +47,7 @@ const FloatingPlusOne:FC<Props> = ({helixColor, pointsNo}) => {
         </span>
       ))}
       <div className="flex items-center justify-center">
-        <img src={sapiensRunning} alt="" width={300} />
+        <img src={sapiensRunning} alt="" className="mx-auto w-[70%]" />
         {/*<l-quantum
           size={'150'}
           speed="5" 
