69a96e4fdee74d1d24e7f2b28c6a3d49a6a4cf0d
Reiz Janto <betswanky@gmail.com>
2024-06-24 18:59:41 +0100

    Rewrite ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,12 @@
 module.exports = {
-    apps: [
-      {
-        name: "vite-react-app",
-        script: "serve",
-        args: "-s dist -l 3000", // -s for serving static files, -l for specifying the port
-        env: {
-          NODE_ENV: "production"
-        }
+  apps: [
+    {
+      name: "vite-react-app",
+      script: "serve",
+      args: ["-s", "dist", "-l", "3000"],
+      env: {
+        NODE_ENV: "production"
       }
-    ]
+    }
+  ]
 };
-  
