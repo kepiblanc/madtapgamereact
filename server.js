ad975ec61421e6f0024f0afc26a8a048e5d128d5
Reiz Janto <betswanky@gmail.com>
2024-06-24 19:35:44 +0100

    Add express rewrite

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
